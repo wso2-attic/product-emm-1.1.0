670bd4de077a69aaebc47289ad0d752e6e5bc535
Chan <dulithaz@gmail.com>
2014-03-28 09:16:10 +0530

    The IP related fixes that fix how the ip address and hostname is obtained for the server

--- modules/apps/mdm/config/mdm.js
+++ modules/apps/mdm/config/mdm.js
@@ -1,24 +1,21 @@
 var config;
 (function () {
+    /* 
+        Will be removing the confusion with the host and ip. 
+        If carbon.xml hostname is available it will use it. 
+        Otherwise it will use the ip. 
+    */
     config = function () {
         var log = new Log(),
             pinch = require('/modules/pinch.min.js').pinch,
             config = require('/config/config.json'),
-            process = require('process'),
-            localIP = process.getProperty('carbon.local.ip'),
-            host = process.getProperty('server.host'),
-            httpPort = process.getProperty('http.port'),
-            httpsPort = process.getProperty('https.port');
+            address = require('/modules/address.js');
 
         pinch(config, /^/, function (path, key, value) {
             if ((typeof value === 'string') && value.indexOf('%https.ip%') > -1) {
-                return value.replace('%https.ip%', 'https://' + localIP + ':' + httpsPort);
-            } else if ((typeof value === 'string') && value.indexOf('%http.ip%') > -1) {
-                return value.replace('%http.ip%', 'http://' + localIP + ':' + httpPort);
-            }else if ((typeof value === 'string') && value.indexOf('%https.host%') > -1) {
-                return value.replace('%https.host%', 'https://' + host);
-            }else if ((typeof value === 'string') && value.indexOf('%http.host%') > -1) {
-                return value.replace('%http.host%', 'http://' + host);
+                return value.replace('%https.ip%', address.getAddress("https"));
+            }else if ((typeof value === 'string') && value.indexOf('%http.ip%') > -1) {
+                return value.replace('%http.ip%', address.getAddress("http"));
             }
             return  value;
         });
