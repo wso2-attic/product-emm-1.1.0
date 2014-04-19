97e340925e452f9c979971bfd2a9b96a1abdf94a
Chan <dulithaz@gmail.com>
2014-04-19 16:40:13 +0530

    Created a new app for emm refactoring

---
+++ modules/apps/emm/config/init.js
@@ -0,0 +1,3 @@
+/**
+Initilization logic
+**/
---
+++ modules/apps/emm/features/device_management/routers/device_router.js
@@ -0,0 +1,6 @@
+var device_router = function(router){
+	router.get('/device/register/:id', function(req, res){
+		var id = req.params.id;
+		// call in device_module to get a device object
+	});
+}
