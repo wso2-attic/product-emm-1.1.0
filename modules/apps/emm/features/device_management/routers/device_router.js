3367ea85b37535f4cb460f05a00de07439c0aae6
Chan <dulithaz@gmail.com>
2014-04-19 16:42:08 +0530

    Fixed the logic for running routes

--- modules/apps/emm/features/device_management/routers/device_router.js
+++ modules/apps/emm/features/device_management/routers/device_router.js
@@ -1,4 +1,4 @@
-var device_router = function(router){
+var api_router = function(router){
 	router.get('/device/register/:id', function(req, res){
 		var id = req.params.id;
 		// call in device_module to get a device object
