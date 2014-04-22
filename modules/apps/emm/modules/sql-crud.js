a8db045ce49c5d961f24f7adfbf1784ace4720bd
Chan <dulithaz@gmail.com>
2014-04-22 08:54:28 +0530

    Cleaned the sql-crud plugin for cleaner implementation

--- modules/apps/emm/modules/sql-crud.js
+++ modules/apps/emm/modules/sql-crud.js
@@ -2,6 +2,9 @@ var sqlCRUD = function (schema, options) {
     var log = new Log('sql-crud');
     var db = options.db;
 
+    /*
+        Transform resultset to entity list
+    */
     var transform = function(results){
         var props = schema.props; 
         var list = [];
@@ -19,52 +22,44 @@ var sqlCRUD = function (schema, options) {
         return list;
     }
 
+    var removeLastComma = function(query){
+        return query.substring(0, query.length - 1);;
+    }
+     var removeLastAnd = function(query){
+        return query.substring(0, query.length - 3);;
+    }
+
     schema.static.find = function (query, pagination) {
-        try{
-            var types = Object.getOwnPropertyNames(query);
-            var query_line = "SELECT * FROM "+schema.meta.tablename+" WHERE ";
-            var where = "";
-            for (var i = types.length - 1; i >= 0; i--) {
-                where += " "+types[i]+"='"+query[types[i]]+"' and";
-            };
-            where = where.substring(0, where.length - 3);
-            query_line = query_line+where;
-            log.info(query_line);
-            var results = db.query(query_line);
-            var list = transform(results);
-        }catch(e){
-            log.info(e);
-        }
+        var types = Object.getOwnPropertyNames(query);
+        var query_line = "SELECT * FROM `"+schema.meta.tablename+"` WHERE ";
+        var where = "";
+        for (var i = types.length - 1; i >= 0; i--) {
+            where += " "+types[i]+"='"+query[types[i]]+"' and";
+        };
+        query_line += removeLastAnd(where);
+        log.info('Find Query:- '+query_line);
+        var list = transform(db.query(query_line));
         return list;
     };
     schema.static.findAll = function(){
-        try{
-            var query_line = "SELECT * FROM "+schema.meta.tablename;
-            log.info(query_line);
-            var results = db.query(query_line);
-            var list = transform(results);
-        }catch(e){
-            log.info(e);
-        }
+        var query_line = "SELECT * FROM `"+schema.meta.tablename+"`";
+        log.info('FindAll Query:- '+query_line);
+        var list = transform(db.query(query_line));
         return list;
     }
-
+    /* 
+        Will throw exception if more than one result is available for query
+    */
     schema.static.findOne = function (query) {
-        try{
-            var types = Object.getOwnPropertyNames(query);
-            var query_line = "SELECT * FROM "+schema.meta.tablename+" WHERE ";
-            var where = "";
-            for (var i = types.length - 1; i >= 0; i--) {
-                where += " "+types[i]+"='"+query[types[i]]+"' and";
-            };
-            where = where.substring(0, where.length - 3);
-            query_line = query_line+where;
-            log.info(query_line);
-            var results = db.query(query_line);
-            var list = transform(results);
-        }catch(e){
-            log.info(e);
-        }
+        var types = Object.getOwnPropertyNames(query);
+        var query_line = "SELECT * FROM `"+schema.meta.tablename+"` WHERE ";
+        var where = "";
+        for (var i = types.length - 1; i >= 0; i--) {
+            where += " "+types[i]+"='"+query[types[i]]+"' and";
+        };
+        query_line += removeLastAnd(where);
+        log.info('FindOne Query:- '+query_line);
+        var list = transform(db.query(query_line));
         if(list.length>1){
             throw "One than one result found";
         }
@@ -76,30 +71,24 @@ var sqlCRUD = function (schema, options) {
     });
 
     schema.to('save', function (entity, next) {
-        try{
-            // log.info(entity);
-            log.info(schema.props);
-            var props = schema.props;
-            var query = "INSERT INTO `"+schema.meta.tablename+"`";
-            var keys = Object.getOwnPropertyNames(entity);
-            var columns = "(";
-            var values = "(";
-            for (var i = keys.length - 1; i >= 0; i--) {
-                var column = keys[i];
-                var columnType = props[column].type;
-                columns += String.toUpperCase(column)+","
-                values += "'"+entity[column]+"',";
-            };
-            columns = columns.substring(0, columns.length - 1) +")";
-            values = values.substring(0, values.length - 1) + ")";
-            query +=columns+" VALUES "+values;
-            //log.info(query);
-            db.query(query);
-            // var results = db.query("SELECT * FROM `platforms`");
-            // db.query("CREATE TABLE `devices` (`id` int(11) NOT NULL AUTO_INCREMENT,`name` varchar(45) DEFAULT NULL, `description` varchar(45) DEFAULT NULL, `registrationDate` timestamp DEFAULT NULL)");
-        }catch(e){
-            log.info(e);
-        }
+        var props = schema.props;
+        var query = "INSERT INTO `"+schema.meta.tablename+"`";
+        var keys = Object.getOwnPropertyNames(entity);
+        //Add starting brackets
+        var columns = "(";
+        var values = "(";
+        for (var i = keys.length - 1; i >= 0; i--) {
+            var column = keys[i];
+            // var columnType = props[column].type;
+            columns += String.toUpperCase(column)+","
+            values += "'"+entity[column]+"',";
+        };
+        //Add ending brackets
+        columns = removeLastComma(columns) +")";
+        values = removeLastComma(values) + ")";
+
+        query +=columns+" VALUES "+values;
+        db.query(query);
     });
 
     schema.pre('init', function (entity, next) {
