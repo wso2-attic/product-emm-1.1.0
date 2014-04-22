ee98421471b238987e215742e282255e802ca55c
Chan <dulithaz@gmail.com>
2014-04-22 09:06:10 +0530

    Cleaned up the example usage in device router

--- modules/apps/emm/features/device_management/routers/device_router.js
+++ modules/apps/emm/features/device_management/routers/device_router.js
@@ -1,13 +1,14 @@
 var api_router = function(router){
 	var entity = require('entity');
+	var log = new Log('device-router');
 
     // var moment = require("modules/moment.js").moment();
 	var sql_crud = require('/modules/sql-crud.js').sqlCRUD;
 	var Device = new entity.Schema("Device", {
-	  id:Number,
-	  name:String,
-	  description:String,
-	  registrationDate:Date
+	  id : Number,
+	  name : String,
+	  description : String,
+	  registrationDate : Date
 	}, {
 		tablename : "devices"
 	});
@@ -25,23 +26,24 @@ var api_router = function(router){
 		// call in device_module to get a device object
 	});
 	router.post('/api/device/register/', function(req, res){
-		var log = new Log();
-		log.info("Dammit");
+		// Create new Device entity object
 		var Device=entity.model('Device');
 		var device_1 = new Device();
-		device_1.id = 11;
+		device_1.id = 12;
 		device_1.name = "Chan";
 		device_1.description = "Damn right!";
-		// try{
-		// 	// var bi = moment(new Date()).format('YYYY-MM-DD hh:mm:ss.SS');
-		// 	log.info(bi);
-		// }catch(e){
-		// 	log.info(e);
-		// }
 		device_1.registrationDate = "2012-03-02 08:07:23.234";
+		//Persist to DB
 		device_1.save();
+
+		// Read multiple devices
 		var results = Device.find({"name":"Chan"});
-		var results = Device.findOne({"id":"11"});
-		log.info(results);
+		var results = Device.findAll();
+		try{
+			// Get one device. Will throw exception if more than one found
+			var device = Device.findOne({"id":"12"});
+		}catch(e){
+			log.error(e);
+		}
 	});
 }
