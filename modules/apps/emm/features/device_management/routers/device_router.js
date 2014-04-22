ef517054661cd9de875de73af384c1cb43a9258d
Chan <dulithaz@gmail.com>
2014-04-22 08:30:37 +0530

    Created the sql plugin. It supports saving, querying

--- modules/apps/emm/features/device_management/routers/device_router.js
+++ modules/apps/emm/features/device_management/routers/device_router.js
@@ -1,8 +1,47 @@
 var api_router = function(router){
-	router.get('/device/register/:id', function(req, res){
+	var entity = require('entity');
+
+    // var moment = require("modules/moment.js").moment();
+	var sql_crud = require('/modules/sql-crud.js').sqlCRUD;
+	var Device = new entity.Schema("Device", {
+	  id:Number,
+	  name:String,
+	  description:String,
+	  registrationDate:Date
+	}, {
+		tablename : "devices"
+	});
+	var db = application.get("db");
+	if(!db){
+		db = new Database("JAGH2")
+		application.put("db", db);
+	}
+	Device.plugin(sql_crud, {db: db});
+
+	router.get('/api/device/register/:id', function(req, res){
 		var id = req.params.id;
 		var log = new Log();
 		log.info(id);
 		// call in device_module to get a device object
+	});
+	router.post('/api/device/register/', function(req, res){
+		var log = new Log();
+		log.info("Dammit");
+		var Device=entity.model('Device');
+		var device_1 = new Device();
+		device_1.id = 11;
+		device_1.name = "Chan";
+		device_1.description = "Damn right!";
+		// try{
+		// 	// var bi = moment(new Date()).format('YYYY-MM-DD hh:mm:ss.SS');
+		// 	log.info(bi);
+		// }catch(e){
+		// 	log.info(e);
+		// }
+		device_1.registrationDate = "2012-03-02 08:07:23.234";
+		device_1.save();
+		var results = Device.find({"name":"Chan"});
+		var results = Device.findOne({"id":"11"});
+		log.info(results);
 	});
 }
--- modules/apps/emm/modules/sql-crud.js
+++ modules/apps/emm/modules/sql-crud.js
@@ -1,47 +1,74 @@
 var sqlCRUD = function (schema, options) {
-
     var log = new Log('sql-crud');
+    var db = options.db;
 
-    var populateRXTFields = function (entity, options) {
-        var type = entity.type;
-
-        if (type === '') {
-            log.debug('type not specified');
-            return;
-        }
-
-        schema.addToEntity({
-            attributes: {
-                overview_name: String
-            }
-        }, entity);
-
-    };
-
-    schema.static.find = function (type, query, pagination) {
-        var db = options.db;
-        var assets= assetManager.search(type, query||null, pagination||null);
+    var transform = function(results){
+        var props = schema.props; 
+        var list = [];
         var Model=schema.getModel();
-        var items=[];
-        var model;
-        var id,type,path,lifecycle,lifecycleState,mediaType;
+        for (var i = results.length - 1; i >= 0; i--) {
+            var result = results[i];
+            var obj = new Model();
+            var fields = Object.getOwnPropertyNames(props);
+            for (var j = fields.length - 1; j >= 0; j--) {
+                var field = fields[j];
+                obj[field] = result[field.toUpperCase()];
+            };
+            list.push(obj);
+        };
+        return list;
+    }
 
-        for(var index in assets){
-            id=assets[index].id;
-            type=assets[index].type;
-            lifecycle=assets[index].lifecycle;
-            lifecycleState=assets[index].lifecycleState;
-            mediaType=assets[index].mediaType;
-            model=new Model({attributes:assets[index].attributes});
-            items.push(model);
+    schema.static.find = function (query, pagination) {
+        try{
+            var types = Object.getOwnPropertyNames(query);
+            var query_line = "SELECT * FROM "+schema.meta.tablename+" WHERE ";
+            var where = "";
+            for (var i = types.length - 1; i >= 0; i--) {
+                where += " "+types[i]+"='"+query[types[i]]+"' and";
+            };
+            where = where.substring(0, where.length - 3);
+            query_line = query_line+where;
+            log.info(query_line);
+            var results = db.query(query_line);
+            var list = transform(results);
+        }catch(e){
+            log.info(e);
         }
-
-        return items;
+        return list;
     };
+    schema.static.findAll = function(){
+        try{
+            var query_line = "SELECT * FROM "+schema.meta.tablename;
+            log.info(query_line);
+            var results = db.query(query_line);
+            var list = transform(results);
+        }catch(e){
+            log.info(e);
+        }
+        return list;
+    }
 
-    schema.static.findOne = function (type, query, pagination) {
-        var assetManager = options.assetManager();
-        return assetManager.search(type, query, pagination);
+    schema.static.findOne = function (query) {
+        try{
+            var types = Object.getOwnPropertyNames(query);
+            var query_line = "SELECT * FROM "+schema.meta.tablename+" WHERE ";
+            var where = "";
+            for (var i = types.length - 1; i >= 0; i--) {
+                where += " "+types[i]+"='"+query[types[i]]+"' and";
+            };
+            where = where.substring(0, where.length - 3);
+            query_line = query_line+where;
+            log.info(query_line);
+            var results = db.query(query_line);
+            var list = transform(results);
+        }catch(e){
+            log.info(e);
+        }
+        if(list.length>1){
+            throw "One than one result found";
+        }
+        return list;
     };
 
     schema.pre('save', function (entity, next) {
@@ -49,14 +76,33 @@ var sqlCRUD = function (schema, options) {
     });
 
     schema.to('save', function (entity, next) {
-        log.info('Saving the entity');
-        entity = entity.toJSON();
-        var assetManager = options.assetManager();
-        assetManager.add(entity, options);
+        try{
+            // log.info(entity);
+            log.info(schema.props);
+            var props = schema.props;
+            var query = "INSERT INTO `"+schema.meta.tablename+"`";
+            var keys = Object.getOwnPropertyNames(entity);
+            var columns = "(";
+            var values = "(";
+            for (var i = keys.length - 1; i >= 0; i--) {
+                var column = keys[i];
+                var columnType = props[column].type;
+                columns += String.toUpperCase(column)+","
+                values += "'"+entity[column]+"',";
+            };
+            columns = columns.substring(0, columns.length - 1) +")";
+            values = values.substring(0, values.length - 1) + ")";
+            query +=columns+" VALUES "+values;
+            //log.info(query);
+            db.query(query);
+            // var results = db.query("SELECT * FROM `platforms`");
+            // db.query("CREATE TABLE `devices` (`id` int(11) NOT NULL AUTO_INCREMENT,`name` varchar(45) DEFAULT NULL, `description` varchar(45) DEFAULT NULL, `registrationDate` timestamp DEFAULT NULL)");
+        }catch(e){
+            log.info(e);
+        }
     });
 
     schema.pre('init', function (entity, next) {
-        populateRXTFields(entity);
     });
 
 };
