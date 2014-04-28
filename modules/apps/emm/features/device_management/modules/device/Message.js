b7d2f024bf651ae840d366402750378f37ff3999
Dulitha Wijewantha <dulithaz@gmail.com>
2014-04-28 16:34:17 +0530

    Added the queue method to the Message object

--- modules/apps/emm/features/device_management/modules/device/Message.js
+++ modules/apps/emm/features/device_management/modules/device/Message.js
@@ -1,5 +1,8 @@
 var Message = function(device, operation, options){
+	// Persist the notifier of the message to the device_info table
+	this.queue = function(){
 
+	}
 }
 // figure of the eodmessage
 var EODMessage = function(device){
