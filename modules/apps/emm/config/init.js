26072612236fa153623ff657e3503e7d973a7e6b
Dulitha Wijewantha <dulithaz@gmail.com>
2014-05-05 11:38:41 +0530

    Added hostname work

--- modules/apps/emm/config/init.js
+++ modules/apps/emm/config/init.js
@@ -1,6 +1,39 @@
 /**
 Initilization logic
 **/
+var carbon = require('carbon');
 
 var schema = require('/modules/schema.js');
 var lang = require('/config/lang/locale_en.json');
+/* 
+	Get the relevant configurations from carbon.xml	
+*/
+var conf = carbon.server.loadConfig('carbon.xml');
+var offset = conf.*::['Ports'].*::['Offset'].text();
+var hostName = conf.*::['HostName'].text().toString();
+
+if (hostName === null || hostName === '') {
+    hostName = 'localhost';
+}
+
+var httpPort = 9763 + parseInt(offset, 10);
+var httpsPort = 9443 + parseInt(offset, 10);
+
+	if(transport=="http"){
+		port = process.getProperty('mgt.transport.http.proxyPort');
+		if(!port){
+			//can use http.port as well
+			port = process.getProperty('mgt.transport.http.port');
+		}
+	}else if(transport=="https"){
+		port = process.getProperty('mgt.transport.https.proxyPort');
+		if(!port){
+			//can use https.port as well
+			port = process.getProperty('mgt.transport.https.port');
+		}
+	}
+
+var process = require('process');
+process.setProperty('server.host', hostName);
+process.setProperty('http.port', httpPort.toString());
+process.setProperty('https.port', httpsPort.toString());
