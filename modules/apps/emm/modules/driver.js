636fcef6c73e3a95bafa9ac2ae1edeac4b81b8fc
gayan <gayan@gayanpc.(none)>
2014-05-12 19:27:23 +0530

    fix permissions

--- modules/apps/emm/modules/driver.js
+++ modules/apps/emm/modules/driver.js
@@ -1,41 +1,43 @@
 /* 
-    Usage - 
-        var driver = require('driver.js').driver(db);
-        driver.query();
-*/
-var driver = function(db){
-    var translate = function(results){
+ Usage -
+ var driver = require('driver.js').driver(db);
+ driver.query();
+ */
+var driverObject = {};
+var driver = function(db) {
+    driverObject.translate = function(results) {
         var models = [];
         for (var i = results.length - 1; i >= 0; i--) {
             var result = results[i];
             var changed = {};
             for (var prop in result) {
                 if (result.hasOwnProperty(prop)) {
-                    prop = prop..toLowerCase();
-                    if(result[field] == null) {
-                        changed[prop] = result[prop];
+                    //prop = prop.toLowerCase();
+                    if (result[prop] == null) {
+                        changed[prop.toLowerCase()] = result[prop];
                     } else {
-                        changed[prop] = result[prop].toString();
+                        changed[prop.toLowerCase()] = result[prop].toString();
                     }
-                    models.push(changed);
                 }
             }
+            models.push(changed);
         };
         return models;
     }
-    this.query = function(){
+    driverObject.query = function() {
         // convert arguments to array
         var args = Array.prototype.slice.call(arguments, 0);
-        var query = args.shift();
-        if (args.length>0) {
+        var query = args[0];
+        if (args.length > 1) {
             result = db.query.apply(db, args) || [];
-        }
-        else {
+        } else {
             result = db.query(query) || [];
         }
-        var processed = translate(result);
+        var processed = result;
+        if(Object.prototype.toString.call( processed ) === '[object Array]'){
+            processed = driverObject.translate(result);
+        }
         return processed;
     }
+    return driverObject;
 }
-
- 
