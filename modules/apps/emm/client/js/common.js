8c8b9f93262633a95b2ea2475dd479c46453500b
Chatura Dilan Perera <dilan@dilan.me>
2014-05-12 15:15:29 +0530

    Added Change password UI

--- modules/apps/emm/client/js/common.js
+++ modules/apps/emm/client/js/common.js
@@ -4,38 +4,38 @@ var apiConfig = getApiConfig();
 var uiConfig = getUIConfig('default');
 
 $.get('/emm/console/info').done(function(data) {
-			tenantDomain = JSON.parse(data).tenantDomain;
-			var uiConfig = getUIConfig('default');
-		}).fail(function() {
+    tenantDomain = JSON.parse(data).tenantDomain;
+    var uiConfig = getUIConfig('default');
+}).fail(function() {
 
 });
 
 
 
 function getAppConfig(){
-	var appConfig = loadTextFileAjaxSync("/emm/config/config.json", "application/json");
-	return JSON.parse(appConfig);
+    var appConfig = loadTextFileAjaxSync("/emm/config/config.json", "application/json");
+    return JSON.parse(appConfig);
 }
 
 function getUIConfig(tenantDomain){
-	var uiConfig = loadTextFileAjaxSync("/emm/config/tenants/" + tenantDomain +  "/ui.json", "application/json");
-	return JSON.parse(uiConfig);
+    var uiConfig = loadTextFileAjaxSync("/emm/config/tenants/" + tenantDomain +  "/ui.json", "application/json");
+    return JSON.parse(uiConfig);
 }
 
 function getApiConfig(){	
-	var apiConfig = loadTextFileAjaxSync("/emm/config/apis.json", "application/json");
-	return JSON.parse(apiConfig);
+    var apiConfig = loadTextFileAjaxSync("/emm/config/apis.json", "application/json");
+    return JSON.parse(apiConfig);
 }
 
 
 context = function() {	
-	
-	var appDefault = {				
-		resourcePath: uiConfig.EMM_UI_URI + "themes/" + uiConfig.EMM_THEME + "/img/",
-		serverURL: uiConfig.EMM_API_URI,
-		appsImageService: uiConfig.APPS_IMAGE_SERVICE
-	};
-	return appDefault;
+
+    var appDefault = {				
+        resourcePath: uiConfig.EMM_UI_URI + "themes/" + uiConfig.EMM_THEME + "/img/",
+        serverURL: uiConfig.EMM_API_URI,
+        appsImageService: uiConfig.APPS_IMAGE_SERVICE
+    };
+    return appDefault;
 };
 
 
@@ -43,89 +43,89 @@ context = function() {
 
 function loadTextFileAjaxSync(filePath, mimeType)
 {
-  var xmlhttp=new XMLHttpRequest();
-  xmlhttp.open("GET",filePath,false);
-  if (mimeType != null) {
-    if (xmlhttp.overrideMimeType) {
-      xmlhttp.overrideMimeType(mimeType);
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.open("GET",filePath,false);
+    if (mimeType != null) {
+        if (xmlhttp.overrideMimeType) {
+            xmlhttp.overrideMimeType(mimeType);
+        }
     }
-  }
-  xmlhttp.send();
-  if (xmlhttp.status==200)
-  {
-    return xmlhttp.responseText;
-  }
-  else {
-    // TODO Throw exception
-    return null;
-  }
+    xmlhttp.send();
+    if (xmlhttp.status==200)
+    {
+        return xmlhttp.responseText;
+    }
+    else {
+        // TODO Throw exception
+        return null;
+    }
 }
 
 
 
 getServiceURLs = function(item){	
-	
-	var tenantId = null;
-	$.get('/emm/console/info').done(function(data) {
-			tenantId = JSON.parse(data).tenantId;
-			
-		}).fail(function() {
 
-	});
+    var tenantId = null;
+    $.get('/emm/console/info').done(function(data) {
+        tenantId = JSON.parse(data).tenantId;
 
-	var serverURL = context().serverURL;	
-	var urls = apiConfig.APIS;		
-	arguments[0] = urls[item];	
-	if(tenantId){
-		return serverURL + String.format.apply(this, arguments) + "&tenantId=" + tenantId;
-	}else{
-		return serverURL + String.format.apply(this, arguments);
-	}	
+    }).fail(function() {
+
+    });
+
+    var serverURL = context().serverURL;	
+    var urls = apiConfig.APIS;		
+    arguments[0] = urls[item];	
+    if(tenantId){
+        return serverURL + String.format.apply(this, arguments) + "&tenantId=" + tenantId;
+    }else{
+        return serverURL + String.format.apply(this, arguments);
+    }	
 
 };
 
 
 
 String.format = function() {
-  var s = arguments[0]; 
-  for (var i = 0; i < arguments.length - 1; i++) {       
-    var reg = new RegExp("\\{" + i + "\\}", "gm");             
-    s = s.replace(reg, arguments[i + 1]);
-  }
+    var s = arguments[0]; 
+    for (var i = 0; i < arguments.length - 1; i++) {       
+        var reg = new RegExp("\\{" + i + "\\}", "gm");             
+        s = s.replace(reg, arguments[i + 1]);
+    }
 
-  return s;
+    return s;
 }
 
 
 $('.datepicker').datepickerbt({
-				format: 'yyyy-mm-dd'
-			});
+    format: 'yyyy-mm-dd'
+});
 
 
 $.noty.defaults = {
-	layout: 'center',
-	theme: 'defaultTheme',
-	type: 'alert',
-	text: '',	
-	dismissQueue: true, // If you want to use queue feature set this true
-	template: '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>',
-	animation: {
-		open: {height: 'toggle'},
-		close: {height: 'toggle'},
-		easing: 'swing',
-		speed: 100 // opening & closing animation speed
-	},
-	timeout: 1000, // delay for closing event. Set false for sticky notifications
-	force: false, // adds notification to the beginning of queue when set to true
-	modal: true,
-	closeWith: ['click'], // ['click', 'button', 'hover']
-	callback: {
-		onShow: function() {},
-		afterShow: function() {},
-		onClose: function() {},
-		afterClose: function() {}
-	},
-	buttons: false // an array of buttons
+    layout: 'center',
+    theme: 'defaultTheme',
+    type: 'alert',
+    text: '',	
+    dismissQueue: true, // If you want to use queue feature set this true
+    template: '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>',
+    animation: {
+        open: {height: 'toggle'},
+        close: {height: 'toggle'},
+        easing: 'swing',
+        speed: 100 // opening & closing animation speed
+    },
+    timeout: 1000, // delay for closing event. Set false for sticky notifications
+    force: false, // adds notification to the beginning of queue when set to true
+    modal: true,
+    closeWith: ['click'], // ['click', 'button', 'hover']
+    callback: {
+        onShow: function() {},
+        afterShow: function() {},
+        onClose: function() {},
+        afterClose: function() {}
+    },
+    buttons: false // an array of buttons
 };
 
 
@@ -136,55 +136,55 @@ $('.selectpicker').selectpicker();
 $('.duallistbox').bootstrapDualListbox();
 $(".dropdownimage").msDropDown();
 $('.multiselect').multiselect({
-			        	includeSelectAllOption: true,
-			        	enableFiltering: 1,
-			        	maxHeight: 150,
-			        	buttonWidth: "215px"
+    includeSelectAllOption: true,
+    enableFiltering: 1,
+    maxHeight: 150,
+    buttonWidth: "215px"
 });
 
 
 $('.nav-tabs a').click(function(e) {
-	e.preventDefault();
-	$(this).tab('show');
+    e.preventDefault();
+    $(this).tab('show');
 });
 
 $(".jtootip").tooltip();
 
 
 $(".als-container").als({
-	visible_items: 20,
-	scrolling_items: 2,
-	orientation: "horizontal",
-	circular: "yes",
-	autoscroll: "yes",
-	interval: 6000,
-	direction: "right",
-	start_from: 1
+    visible_items: 20,
+    scrolling_items: 2,
+    orientation: "horizontal",
+    circular: "yes",
+    autoscroll: "yes",
+    interval: 6000,
+    direction: "right",
+    start_from: 1
 });
 
 
 Handlebars.registerHelper('elipsis', function(maxLength, context, options) {
-  if(context.length > maxLength) {
-    context = context.substring(0, maxLength)+"...";
-  }
-  
-  return context;
+    if(context.length > maxLength) {
+        context = context.substring(0, maxLength)+"...";
+    }
+
+    return context;
 });
 
 
 Handlebars.registerHelper('batteryLevel', function(level, options) {
- if(level < 20){
- 	return "20";
- }else if(level < 40){
- 	return "40";
- }else if(level < 60){
- 	return "60";
- }else if(level < 80){
- 	return "80";
- }else if(level <= 100){
- 	return "100";
- }
- 
+    if(level < 20){
+        return "20";
+    }else if(level < 40){
+        return "40";
+    }else if(level < 60){
+        return "60";
+    }else if(level < 80){
+        return "80";
+    }else if(level <= 100){
+        return "100";
+    }
+
 });
 
 
@@ -225,13 +225,13 @@ Handlebars.registerHelper('compare', function(lvalue, rvalue, options) {
 //DATA TABLES
 
 function createFilter(oTable, rowId, selectId, heading){
-	$('.tabel-filter-group').append('<span class="select-filter">' + fnCreateSelect( oTable.fnGetColumnData(rowId), selectId, heading) + '</span>');	
+    $('.tabel-filter-group').append('<span class="select-filter">' + fnCreateSelect( oTable.fnGetColumnData(rowId), selectId, heading) + '</span>');	
 }
 
 
 
 (function($) {
-	/*
+    /*
 	 * Function: fnGetColumnData
 	 * Purpose:  Return an array of table values from a particular column.
 	 * Returns:  array string: 1d data array
@@ -242,47 +242,47 @@ function createFilter(oTable, rowId, selectId, heading){
 	 *           bool:bIgnoreEmpty - optional - if set to false empty values are not filtered from the result array
 	 * Author:   Benedikt Forchhammer <b.forchhammer /AT\ mind2.de>
 	 */
-	$.fn.dataTableExt.oApi.fnGetColumnData = function ( oSettings, iColumn, bUnique, bFiltered, bIgnoreEmpty ) {
-	    // check that we have a column id
-	    if ( typeof iColumn == "undefined" ) return new Array();
-	     
-	    // by default we only want unique data
-	    if ( typeof bUnique == "undefined" ) bUnique = true;
-	     
-	    // by default we do want to only look at filtered data
-	    if ( typeof bFiltered == "undefined" ) bFiltered = true;
-	     
-	    // by default we do not want to include empty values
-	    if ( typeof bIgnoreEmpty == "undefined" ) bIgnoreEmpty = true;
-	     
-	    // list of rows which we're going to loop through
-	    var aiRows;
-	     
-	    // use only filtered rows
-	    if (bFiltered == true) aiRows = oSettings.aiDisplay;
-	    // use all rows
-	    else aiRows = oSettings.aiDisplayMaster; // all row numbers
-	 
-	    // set up data array   
-	    var asResultData = new Array();
-	     
-	    for (var i=0,c=aiRows.length; i<c; i++) {
-	        iRow = aiRows[i];
-	        var aData = this.fnGetData(iRow);
-	        var sValue = aData[iColumn];
-	         
-	        // ignore empty values?
-	        if (bIgnoreEmpty == true && sValue.length == 0) continue;
-	 
-	        // ignore unique values?
-	        else if (bUnique == true && jQuery.inArray(sValue, asResultData) > -1) continue;
-	         
-	        // else push the value onto the result data array
-	        else asResultData.push(sValue);
-	    }
-	     
-	    return asResultData;
-	}}(jQuery));
+    $.fn.dataTableExt.oApi.fnGetColumnData = function ( oSettings, iColumn, bUnique, bFiltered, bIgnoreEmpty ) {
+        // check that we have a column id
+        if ( typeof iColumn == "undefined" ) return new Array();
+
+        // by default we only want unique data
+        if ( typeof bUnique == "undefined" ) bUnique = true;
+
+        // by default we do want to only look at filtered data
+        if ( typeof bFiltered == "undefined" ) bFiltered = true;
+
+        // by default we do not want to include empty values
+        if ( typeof bIgnoreEmpty == "undefined" ) bIgnoreEmpty = true;
+
+        // list of rows which we're going to loop through
+        var aiRows;
+
+        // use only filtered rows
+        if (bFiltered == true) aiRows = oSettings.aiDisplay;
+        // use all rows
+        else aiRows = oSettings.aiDisplayMaster; // all row numbers
+
+        // set up data array   
+        var asResultData = new Array();
+
+        for (var i=0,c=aiRows.length; i<c; i++) {
+            iRow = aiRows[i];
+            var aData = this.fnGetData(iRow);
+            var sValue = aData[iColumn];
+
+            // ignore empty values?
+            if (bIgnoreEmpty == true && sValue.length == 0) continue;
+
+            // ignore unique values?
+            else if (bUnique == true && jQuery.inArray(sValue, asResultData) > -1) continue;
+
+            // else push the value onto the result data array
+            else asResultData.push(sValue);
+        }
+
+        return asResultData;
+    }}(jQuery));
 
 
 function fnCreateSelect( aData, selectId, heading )
@@ -311,3 +311,59 @@ function getURLParameter(name) {
         (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
     );
 }
+
+
+
+
+
+$("#modalChangePasswordButton").click(function() {
+    var password = $("#modalChangePasswordTxtPassword").val();
+    var passwordConfirm = $("#modalChangePasswordTxtPasswordConf").val();
+    
+    
+
+    if(password != passwordConfirm){
+        var n = noty({
+            text : 'Password and confirm password fields must be identical',
+            'layout' : 'center',
+            type: 'error'
+
+        });
+    }else{
+        //usersChangePassword
+        $("#modalChangePassword").modal('hide');
+         var n = noty({	});
+
+        jQuery.ajax({
+            url : getServiceURLs("usersChangePassword", ""),
+            type : "POST",		
+            data : JSON.stringify({password: password}),		
+            contentType : "application/json",
+            dataType : "json",
+            statusCode: {
+                400: function() {				
+                    n.setText('Error occured!');	
+                    n.setType('error');
+                    n.setTimeout(1000);			
+                },
+                404: function() {				
+                    n.setText('API not found!');	
+                    n.setType('error');	
+                    n.setTimeout(1000);		
+                },
+                500: function() {				
+                    n.setText('Fatal error occured!');	
+                    n.setType('error');
+                    n.setTimeout(1000);			
+                },
+                200: function() {				
+                    n.setText('Password changed successfully!');	
+                    window.location.assign("configuration");
+                }
+            }				
+        });
+
+    }
+
+
+});    
