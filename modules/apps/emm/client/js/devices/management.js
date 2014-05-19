072b74834b21448b7411c067eb2507ce93cc1c54
Chatura Dilan Perera <dilan@dilan.me>
2014-05-19 20:08:13 +0530

    Added devices delete UI

--- modules/apps/emm/client/js/devices/management.js
+++ modules/apps/emm/client/js/devices/management.js
@@ -10,61 +10,68 @@ selectedDevices = null;
 selectedFeature = null;
 
 oTable = $('#main-table').dataTable({
-	
-			
-	
-		"sDom" : "<'row-fluid'<'span6'><'span6'p>r>t<'row-fluid'>",
-		"bProcessing" : true,
-		"bServerSide" : true,
-		"bFilter" : false,
-		
-		aoColumns: [
-                      
-                      {"bVisible":    false },
-                      {"sWidth": "20%"},
 
-                      {                         
-                        "fnRender": function (oObj)                              
-                        {                           
-                            return "<a href='/emm/users/devices?user="  + oObj.aData[2] + "#device-tab-"+ oObj.aData[0]+"'>"+  oObj.aData[2] +"</a>";
-                        }
-                      },
-                      
-                       null,
-                       null,
-                       null,
-                       
 
-                   ],	
-		"sAjaxSource" : "/emm/api/webconsole/listDevices?",
-		"fnServerParams": function ( aoData ) {
-          	var roles = $('#inputRoles').val();
-			var user = $('#inputUser').val();
-			var ownership = $('#inputOwnership').val();
-			var os = $('#inputOS').val();
-			
-            aoData.push( { "name": "role", "value": roles } );
-            aoData.push( { "name": "username", "value": user } );
-            aoData.push( { "name": "byod", "value": ownership } );
-            aoData.push( { "name": "platform_id", "value": os } );
+
+    "sDom" : "<'row-fluid'<'span6'><'span6'p>r>t<'row-fluid'>",
+    "bProcessing" : true,
+    "bServerSide" : true,
+    "bFilter" : false,
+
+    aoColumns: [
+
+        {"bVisible":    false },
+        {"sWidth": "20%"},
+
+        {                         
+            "fnRender": function (oObj)                              
+            {                           
+                return "<a href='/emm/users/devices?user="  + oObj.aData[2] + "#device-tab-"+ oObj.aData[0]+"'>"+  oObj.aData[2] +"</a>";
+            }
+        },
+
+        null,
+        null,
+        null,
+        {                         
+            "sWidth": "40%",
+            "fnRender": function (oObj)                              
+            {
+                return '<a href="#" class="btn-item-remove" data-item="'+ oObj.aData[0] +'" title="Remove"><i class="icon-remove"> </i> Remove</a>&nbsp;';
+            }
         }
-		
-	});
-	
 
-	
+
+    ],	
+    "sAjaxSource" : "/emm/api/webconsole/listDevices?",
+    "fnServerParams": function ( aoData ) {
+        var roles = $('#inputRoles').val();
+        var user = $('#inputUser').val();
+        var ownership = $('#inputOwnership').val();
+        var os = $('#inputOS').val();
+
+        aoData.push( { "name": "role", "value": roles } );
+        aoData.push( { "name": "username", "value": user } );
+        aoData.push( { "name": "byod", "value": ownership } );
+        aoData.push( { "name": "platform_id", "value": os } );
+    }
+
+});
+
+
+
 
 
 
 jQuery.ajax({
-					url : getServiceURLs("groupsCRUD", ""),
-					type : "GET",
-					async : "false",					
-					contentType : "application/json",
-					dataType : "json",
-					success : function(roles) {
-						
-						/*					 $('#inputRoles')
+    url : getServiceURLs("groupsCRUD", ""),
+    type : "GET",
+    async : "false",					
+    contentType : "application/json",
+    dataType : "json",
+    success : function(roles) {
+
+        /*					 $('#inputRoles')
 					        .textext({
 					            plugins : 'autocomplete tags filter'
 					        })
@@ -74,216 +81,291 @@ jQuery.ajax({
 					                textext = $(e.target).textext()[0],
 					                query = (data ? data.query : '') || ''
 					                ;
-					
+
 					            $(this).trigger(
 					                'setSuggestions',
 					                { result : textext.itemManager().filter(list, query) }
 					            );
 					        });*/
-						
-						
-					}					
+
+
+    }					
 
 });
 
 
 $( document ).ready(function() {
-            
-        jQuery.ajax({
-		url : getServiceURLs("platformCRUD", ""),
-		type : "GET",
-		contentType : "application/json",
-		dataType : "json",
-		success: function(platforms){
+
+    jQuery.ajax({
+        url : getServiceURLs("platformCRUD", ""),
+        type : "GET",
+        contentType : "application/json",
+        dataType : "json",
+        success: function(platforms){
             var html = '<option value="">--All--</option>';
-	       	$.each( platforms, function( key, platform ) {
+            $.each( platforms, function( key, platform ) {
                 //alert(JSON.stringify(platform));
                 html += "<option value='"+ platform.id +"'>" + platform.name + "</option>";
             });
-            
+
             $("#inputOS").html(html);
-	    }		
-	});  
-            
-            
+        }		
+    });  
+
+
 
 });
+
+
+
+
+$( "#main-table" ).on( "click", ".btn-item-remove", function() {
+    var item = $(this).data("item");
+
+    noty({
+        text : 'Are you sure you want delete this device?',
+        buttons : [{
+            addClass : 'btn btn-cancel',
+            text : 'Cancel',
+            onClick : function($noty) {
+                $noty.close();
+
+            }
+
+
+        }, {
+
+            addClass : 'btn btn-orange',
+            text : 'Ok',
+            onClick : function($noty) {
+                 $noty.close();	
+                jQuery.ajax({
+                    url : getServiceURLs("devicesCRUD", item),
+                    type : "DELETE",					
+                    contentType : "text/plain",
+                    statusCode: {
+                        400: function() {
+                            noty({
+                                text : 'Error occured!',
+                                'layout' : 'center',
+                                'type': 'error'
+                            });
+                        },
+                        500: function() {
+                            noty({
+                                text : 'Fatal error occured!',
+                                'layout' : 'center',
+                                'type': 'error'
+                            });
+                        },
+                        403: function() {
+                            noty({
+                                text : 'Error occured!',
+                                'layout' : 'center',
+                                'type': 'error'
+                            });
+                        },
+                        200: function() {
+                            noty({
+                                text : 'device is deleted successfully!',
+                                'layout' : 'center'
+                            });
+                            window.location.assign("configuration");
+                        }
+                    }
+
+
+                }).done(function() {
+                   	 $noty.close();			
+                });
+
+
+            }
+
+        }]
+    }); 	
+
+
+});
+
+
+
 
 
 
 $("#btn-find").click(function() {
-	oTable.fnDraw();
+    oTable.fnDraw();
 });
 
 
 $( "#featureList" ).change(function() {
-	
-	
-	var roles = $('#inputRoles').val();
-	var user = $('#inputUser').val();
-	var ownership = $('#inputOwnership').val();
-	var os = $('#inputOS').val();
-	
-	var feature = $(this).val();
-	selectedFeature = feature;
-	var featureTemplate = $("#featureList option:selected").data('template');
-	
-	//selectedFeatureTemplate = this.options[this.selectedIndex].data('template');
-	//alert(selectedFeatureTemplate);
-	
-	var nFiltered = oTable.fnGetData();
-	
-	var devices = new Array();
-	
-	for(var i = 0; i < nFiltered.length; i++){		
-		if (isNaN(nFiltered[i][0]) == false){
-			devices.push(nFiltered[i][0].toString() );
-			
-		}
-	}
-	
-	if(devices.length == 0){
-		noty({
-					text : 'No devices selected',
-					'layout' : 'center',
-					'modal': false,
-					'type': 'error'
-					
-		});
-		return;
-	}
-			
-	selectedDevices = devices;
-	prePerformOperation(devices, feature, featureTemplate);
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+    var roles = $('#inputRoles').val();
+    var user = $('#inputUser').val();
+    var ownership = $('#inputOwnership').val();
+    var os = $('#inputOS').val();
+
+    var feature = $(this).val();
+    selectedFeature = feature;
+    var featureTemplate = $("#featureList option:selected").data('template');
+
+    //selectedFeatureTemplate = this.options[this.selectedIndex].data('template');
+    //alert(selectedFeatureTemplate);
+
+    var nFiltered = oTable.fnGetData();
+
+    var devices = new Array();
+
+    for(var i = 0; i < nFiltered.length; i++){		
+        if (isNaN(nFiltered[i][0]) == false){
+            devices.push(nFiltered[i][0].toString() );
+
+        }
+    }
+
+    if(devices.length == 0){
+        noty({
+            text : 'No devices selected',
+            'layout' : 'center',
+            'modal': false,
+            'type': 'error'
+
+        });
+        return;
+    }
+
+    selectedDevices = devices;
+    prePerformOperation(devices, feature, featureTemplate);
+
+
+
+
+
+
+
+
+
 });
 
 
 
 function prePerformOperation(devices, feature, featureTemplate) {
 
-	if (featureTemplate != "") {
-		$.get('../client/templates/feature_templates/' + featureTemplate + '.hbs').done(function(templateData) {
-			var template = Handlebars.compile(templateData);
-			$("#featureModal").html(template({
-				feature : feature,
-				resourcePath : context().resourcePath
-			}));
-			$('#featureModal').modal('show');
+    if (featureTemplate != "") {
+        $.get('../client/templates/feature_templates/' + featureTemplate + '.hbs').done(function(templateData) {
+            var template = Handlebars.compile(templateData);
+            $("#featureModal").html(template({
+                feature : feature,
+                resourcePath : context().resourcePath
+            }));
+            $('#featureModal').modal('show');
 
-		}).fail(function() {
+        }).fail(function() {
 
-		});
+        });
 
-	} else {
-		performOperation(devices, feature, {
-			data : {}
-		});
-	}
+    } else {
+        performOperation(devices, feature, {
+            data : {}
+        });
+    }
 
 }
 
 
 function performOperation(devices, feature, params) {
 
-	noty({
-		text : 'Are you sure you want to perform this operation? ' + devices.length + ' devices will be affected',
-		buttons : [{
-			addClass : 'btn btn-cancel',
-			text : 'Cancel',
-			onClick : function($noty) {
-				$noty.close();
+    noty({
+        text : 'Are you sure you want to perform this operation? ' + devices.length + ' devices will be affected',
+        buttons : [{
+            addClass : 'btn btn-cancel',
+            text : 'Cancel',
+            onClick : function($noty) {
+                $noty.close();
 
-			}
-			
-			
-		}, {
-			
-			addClass : 'btn btn-orange',
-			text : 'Ok',
-			onClick : function($noty) {
-
-				jQuery.ajax({
-					url : getServiceURLs("performDevicesOperation", feature),
-					type : "POST",
-					async : "false",
-					data : JSON.stringify({operation: feature, devices:devices, params: params}),
-					contentType : "application/json",
-					dataType : "json",
-					
-
-				});
-
-				noty({
-					text : 'Operation is sent to the devices successfully!',
-					'layout' : 'center',
-					'modal': false
-					
-				});
-
-				$noty.close();
-				
-				$("#featureList").msDropdown().data("dd").setIndexByValue("");
-				$("#featureList").val("");
+            }
 
 
+        }, {
 
-			}
-			
-		}]
-	});
+            addClass : 'btn btn-orange',
+            text : 'Ok',
+            onClick : function($noty) {
+
+                jQuery.ajax({
+                    url : getServiceURLs("performDevicesOperation", feature),
+                    type : "POST",
+                    async : "false",
+                    data : JSON.stringify({operation: feature, devices:devices, params: params}),
+                    contentType : "application/json",
+                    dataType : "json",
+
+
+                });
+
+                noty({
+                    text : 'Operation is sent to the devices successfully!',
+                    'layout' : 'center',
+                    'modal': false
+
+                });
+
+                $noty.close();
+
+                $("#featureList").msDropdown().data("dd").setIndexByValue("");
+                $("#featureList").val("");
+
+
+
+            }
+
+        }]
+    });
 
 }
 
 
 $('#featureModal').on('click', '.feature-command', function(e) {
-	var params = {};
+    var params = {};
 
-	var value = $(this).data('value');
-	if (value != "") {
-		params['function'] = value;
-	}
-	
-	var validationFailed = false;
+    var value = $(this).data('value');
+    if (value != "") {
+        params['function'] = value;
+    }
 
-	$(".feature-input").each(function(index) {
-		if($(this).attr('type') == 'checkbox'){			
-			params[$(this).attr("id")] = $(this).is(':checked');
-		}else{
-			params[$(this).attr("id")] = $(this).val();
-			
-			if($(this).data("required") == true && $(this).val() == ""){
-				validationFailed = true;
-				
-				
-				
-			}
-		}
-	});
-	
-	if(validationFailed){
-			noty({
-					text : 'Please fill required fileds',
-					'layout' : 'center',
-					'modal': false,
-					'type': 'error'
-				});
-			return;	
-	}else{
-		$('#featureModal').modal('hide');
-	}
+    var validationFailed = false;
 
-	performOperation(selectedDevices, selectedFeature, {
-		data : params
-	});
+    $(".feature-input").each(function(index) {
+        if($(this).attr('type') == 'checkbox'){			
+            params[$(this).attr("id")] = $(this).is(':checked');
+        }else{
+            params[$(this).attr("id")] = $(this).val();
+
+            if($(this).data("required") == true && $(this).val() == ""){
+                validationFailed = true;
+
+
+
+            }
+        }
+    });
+
+    if(validationFailed){
+        noty({
+            text : 'Please fill required fileds',
+            'layout' : 'center',
+            'modal': false,
+            'type': 'error'
+        });
+        return;	
+    }else{
+        $('#featureModal').modal('hide');
+    }
+
+    performOperation(selectedDevices, selectedFeature, {
+        data : params
+    });
 
 });
 
