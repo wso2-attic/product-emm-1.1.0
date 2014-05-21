2407f64c4ea38aa861031dc87d6d6aac6e6ccdc4
Chatura Dilan Perera <dilan@dilan.me>
2014-05-21 18:49:49 +0530

    Added UI functionality to get values form tenant configuration API

--- modules/apps/emm/client/js/tenant/configuration.js
+++ modules/apps/emm/client/js/tenant/configuration.js
@@ -1,26 +1,41 @@
 
 $(document).ready(function() { 
-    
-		 $('#tenetForm').ajaxForm(function(e) { 
-            var n = noty({
-					text : 'Settings saved!',
-					'layout' : 'center',
-					timeout: 1000				
-								
-	        });
-             
-        }); 
-    
+
+    $('#tenetForm').ajaxForm(function(e) { 
+        var n = noty({
+            text : 'Settings saved!',
+            'layout' : 'center',
+            timeout: 1000				
+
+        });
+
+    }); 
+
+
+    $.ajax({
+        url: getServiceURLs("tenantConfig", ''),
+        type : "GET",
+
+    }).done(function(data) {
+        data = JSON.parse(data);
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                $("#" + key).val(data[key]);
+            }
+        }
+    });
+
+
 });
 
 
 
 jQuery("#tenetForm").submit(function(e) {
-	
-	       $( 'form').parsley( 'validate' );	
-             if(!$('form').parsley('isValid')){
-                 e.preventDefault();
-             }
+
+    $( 'form').parsley( 'validate' );	
+    if(!$('form').parsley('isValid')){
+        e.preventDefault();
+    }
 });
 
 
@@ -29,5 +44,5 @@ $('#iosAPNSCert').change(function(){
 });
 
 $('#iosMDMCert').change(function(){
-     $('#iosMDMCertModified').val("true");
+    $('#iosMDMCertModified').val("true");
 });
