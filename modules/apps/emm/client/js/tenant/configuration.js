dcd6aea525661ce7c0dba93ff8b3b21a44881d63
Chatura Dilan Perera <dilan@dilan.me>
2014-05-21 14:55:35 +0530

    Added validation message for for settings UI

--- modules/apps/emm/client/js/tenant/configuration.js
+++ modules/apps/emm/client/js/tenant/configuration.js
@@ -1,6 +1,28 @@
+
 $(document).ready(function() { 
     
-    $('#tenetForm').ajaxForm(function() { 
-        alert("Thank you for your submit!"); 
-    }); 
+		 $('#tenetForm').ajaxForm(function(e) { 
+            alert("Thank you for your submit!");
+             
+        }); 
+    
+});
+
+
+
+jQuery("#tenetForm").submit(function(e) {
+	
+	       $( 'form').parsley( 'validate' );	
+             if(!$('form').parsley('isValid')){
+                 e.preventDefault();
+             }
+});
+
+
+$('#iosAPNSCert').change(function(){
+    $('#iosAPNSCertModified').val("true");
+});
+
+$('#iosMDMCert').change(function(){
+     $('#iosMDMCertModified').val("true");
 });
