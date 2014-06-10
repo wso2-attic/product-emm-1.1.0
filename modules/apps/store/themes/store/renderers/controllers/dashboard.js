571d465ad098b0fe77cd4681b573c06ff2ff4f1e
Chatura Dilan Perera <dilan@dilan.me>
2014-06-10 17:10:26 +0530

    EMM-673: removed unwanted print

--- modules/apps/store/themes/store/renderers/controllers/dashboard.js
+++ modules/apps/store/themes/store/renderers/controllers/dashboard.js
@@ -27,7 +27,7 @@ var render = function (theme, data, meta, require) {
 	
      if(data.userAssets){
         for(i = 0; i < data.userAssets.mobileapp.length; i++){
-            print(data.userAssets.mobileapp[i].lifecycleState);
+            //print(data.userAssets.mobileapp[i].lifecycleState);
             if(data.userAssets.mobileapp[i].lifecycleState == 'Unpublished'){
                 delete data.userAssets.mobileapp.splice (i, 1);;
             }
